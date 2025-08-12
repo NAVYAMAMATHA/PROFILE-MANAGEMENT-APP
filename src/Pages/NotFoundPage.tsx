8b84ddc07ec91220dabc7161f0f4f3b937eb6644
NAVYA MAMATHA BALAM <160034879+NAVYAMAMATHA@users.noreply.github.com>
2025-08-12 16:11:21 +0530

    Update NotFoundPage.tsx

--- src/Pages/NotFoundPage.tsx
+++ src/Pages/NotFoundPage.tsx
@@ -1,18 +1,4 @@
-/*import React from "react";
-import { Box, Typography, Button } from "@mui/material";
-import { Link as RouterLink } from "react-router-dom";
 
-const NotFound: React.FC = () => {
-  return (
-    <Box textAlign="center" sx={{ mt: 8 }}>
-      <Typography variant="h3">404</Typography>
-      <Typography variant="h6">Page not found</Typography>
-      <Button component={RouterLink} to="/profile" sx={{ mt: 2 }}>Go to Profile</Button>
-    </Box>
-  );
-};
-
-export default NotFound;*/
 import React from "react";
 import { Box,Typography,Button}from "@mui/material";
 import {Link as link} from "react-router-dom";
