824cfe9f551b6efa92dee35c0830eed7f9d1d20b
NAVYA MAMATHA BALAM <160034879+NAVYAMAMATHA@users.noreply.github.com>
2025-08-12 16:11:55 +0530

    Update store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,4 @@
-/*import { configureStore } from "@reduxjs/toolkit";
-import profileReducer from "../features/profile/profileSlice";
 
-export const store = configureStore({
-  reducer: {
-    profile: profileReducer,
-  },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;*/
 import{ configureStore} from  "@reduxjs/toolkit";
 import profileReducer from "../profile/Slice";
 export const store = configureStore({
