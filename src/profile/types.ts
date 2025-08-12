6eb9154e0d29f84923e3128750a601d6f6700f00
NAVYA MAMATHA BALAM <160034879+NAVYAMAMATHA@users.noreply.github.com>
2025-08-12 16:12:58 +0530

    Update types.ts

--- src/profile/types.ts
+++ src/profile/types.ts
@@ -1,12 +1,4 @@
-/*export interface Profile {
-  id?: number;
-  firstName?: string;
-  lastName?: string;
-  name: string; // full name for display (we'll split)
-  email: string;
-  age?: number | null;
-}
-*/
+
 export interface Profile{
   id?:number;
   firstName?:string;
